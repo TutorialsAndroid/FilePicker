30ef24318f9129f306d8fccf7fe668e6737867d2
A.A. Montaser <58310262+PRO33X@users.noreply.github.com>
2025-03-27 20:03:57 +0200

    Update DialogConfigs.java
    Added CURRENT_DIR_SELECT

--- library/src/main/java/com/developer/filepicker/model/DialogConfigs.java
+++ library/src/main/java/com/developer/filepicker/model/DialogConfigs.java
@@ -5,18 +5,21 @@ package com.developer.filepicker.model;
  */
 public abstract class DialogConfigs {
 
-    public static final int SINGLE_MODE = 0;
-    public static final int MULTI_MODE = 1;
-    public static final int FILE_SELECT = 0;
-    public static final int DIR_SELECT = 1;
-    public static final int FILE_AND_DIR_SELECT = 2;
+// Selection Modes
+	public static final int SINGLE_MODE = 0;
+	public static final int MULTI_MODE = 1;
+// Selection Types
+	public static final int FILE_SELECT = 0;
+	public static final int DIR_SELECT = 1;
+	public static final int FILE_AND_DIR_SELECT = 2;
+	public static final int CURRENT_DIR_SELECT = 3; // On selection the Modes will be ignored
 
-    /*  PARENT_DIRECTORY*/
-    private static final String DIRECTORY_SEPARATOR = "/";
-    private static final String STORAGE_DIR = "sdcard";
+	/*  PARENT_DIRECTORY*/
+	private static final String DIRECTORY_SEPARATOR = "/";
+	private static final String STORAGE_DIR = "sdcard";
 
-    /*  DEFAULT_DIR is the default mount point of the SDCARD. It is the default
-     *  mount point.
-     */
-    public static final String DEFAULT_DIR = DIRECTORY_SEPARATOR + STORAGE_DIR;
+	/*  DEFAULT_DIR is the default mount point of the SDCARD. It is the default
+	 *  mount point.
+	 */
+	public static final String DEFAULT_DIR = DIRECTORY_SEPARATOR + STORAGE_DIR;
 }
